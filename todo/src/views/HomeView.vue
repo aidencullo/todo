2397329816f7963da4923f30c2806642e3a96634
Aiden Cullo <aidencullo@gmail.com>
2023-04-22 16:11:42 -0500

    add basic todo functionality

--- todo/src/views/HomeView.vue
+++ todo/src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <Todo msg="Welcome to Your Vue.js App"/>
+    <TodoList msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Todo from '@/components/Todo.vue'
+import TodoList from '@/components/TodoList.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    Todo
+    TodoList
   }
 }
 </script>
