a97f10defcb90c7247192fa05dd3e6a64c60f403
Aiden Cullo <aidencullo@gmail.com>
2023-04-22 20:50:09 -0500

    recreate app with vite, add tailwind

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,29 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-  </nav>
-  <router-view/>
+    <span class="text-3xl font-bold underline">
+      Hello world!
+    </span>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
