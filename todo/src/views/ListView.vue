a1be1cd3fd52a346b69bf9e0de5302a3df5a713a
Aiden Cullo <aidencullo@gmail.com>
2023-04-23 19:00:58 -0500

    implement basic todo list

--- todo/src/views/ListView.vue
+++ todo/src/views/ListView.vue
@@ -11,4 +11,31 @@
   </span>
 </h1>
 
+
+<input v-model.trim="newItem" placeholder="add new item"
+	       class="border-2 border-black">
+    <button @click="addItem" class="m-2 bg-blue-500 hover:bg-blue-700
+    text-white font-bold py-2 px-4 rounded">Add</button>
+    <ol>
+      <li v-for="(item, index) in items" :key="item.id">
+	{{ index + 1 }} . {{ item }}
+      </li>
+    </ol>
+
 </template>
+
+<script setup>
+  import { ref } from "vue"
+  
+  const items = ref([]);
+  const newItem = ref("");
+  
+  function addItem() {
+  if (newItem.value.length === 0) {
+  return
+  }
+  items.value.push(newItem.value);
+  newItem.value="";
+}
+
+</script>
