695c1c5ea693e5d356a3e48b495197a0d3ed1d5b
Aiden Cullo <aidencullo@gmail.com>
2023-04-24 14:21:48 -0500

    todo items expand when you click on them

--- todo/src/components/TodoList.vue
+++ todo/src/components/TodoList.vue
@@ -5,14 +5,16 @@
 <button @click="addItem" class="m-2 bg-blue-500 hover:bg-blue-700
 				text-white font-bold py-2 px-4 rounded">Add</button>
 <button @click="clearItems" class="m-2 bg-blue-500 hover:bg-blue-700
-				text-white font-bold py-2 px-4 rounded">Clear</button>
+				   text-white font-bold py-2 px-4 rounded">Clear</button>
 
-<div class="text-left bg-green-700 mr-auto ml-auto p-4 w-1/2" v-show="items.length">    
-  <ol>
-    <li v-for="(item, index) in items" :key="item.id">
-      {{ index + 1 }} . {{ item }}
+<div class="text-left bg-green-700 mr-auto ml-auto p-4 w-1/2 break-all" v-show="items.length">    
+  <ul>
+    <li v-for="{ id, item, expand } in items" :key="id">
+      <div @click="toggleExpand(id)" :class="[{truncate:expand}, 'cursor-pointer']">
+	{{ id }} . {{ item }}
+      </div>
     </li>
-  </ol>
+  </ul>
 </div>
 
 </template>
@@ -27,11 +29,19 @@
   if (newItem.value.length === 0) {
   return
   }
-  items.value.push(newItem.value);
+  items.value.push({
+  id : items.value.length + 1,
+  item : newItem.value,
+  expand : true
+  });
   newItem.value="";
   }
 
   function clearItems() {
   items.value = [];
+  }
+
+  function toggleExpand(id) {
+  items.value[Number(id) - 1].expand = !items.value[Number(id) - 1].expand;
   }
 </script>
