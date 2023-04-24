68366eaa323be14a2d93cfc1b6a9a5f267ab991a
Aiden Cullo <aidencullo@gmail.com>
2023-04-24 13:12:36 -0500

    add clear list button

--- todo/src/components/TodoList.vue
+++ todo/src/components/TodoList.vue
@@ -4,8 +4,10 @@
        class="border-2 border-black">
 <button @click="addItem" class="m-2 bg-blue-500 hover:bg-blue-700
 				text-white font-bold py-2 px-4 rounded">Add</button>
+<button @click="clearItems" class="m-2 bg-blue-500 hover:bg-blue-700
+				text-white font-bold py-2 px-4 rounded">Clear</button>
 
-<div class="text-left bg-green-700 mr-auto ml-auto p-4 w-1/2" id="list">    
+<div class="text-left bg-green-700 mr-auto ml-auto p-4 w-1/2" v-show="items.length">    
   <ol>
     <li v-for="(item, index) in items" :key="item.id">
       {{ index + 1 }} . {{ item }}
@@ -27,5 +29,9 @@
   }
   items.value.push(newItem.value);
   newItem.value="";
+  }
+
+  function clearItems() {
+  items.value = [];
   }
 </script>
