64c2378afadfc15b495471d1a4f624ce7b594b6d
Aiden Cullo <aidencullo@gmail.com>
2023-04-24 12:58:18 -0500

    align left todo list items

--- todo/src/components/TodoList.vue
+++ todo/src/components/TodoList.vue
@@ -1,14 +1,17 @@
 <template>
 
 <input v-model.trim="newItem" placeholder="add new item"
-	       class="border-2 border-black">
-    <button @click="addItem" class="m-2 bg-blue-500 hover:bg-blue-700
-    text-white font-bold py-2 px-4 rounded">Add</button>
-    <ol>
-      <li v-for="(item, index) in items" :key="item.id">
-	{{ index + 1 }} . {{ item }}
-      </li>
-    </ol>
+       class="border-2 border-black">
+<button @click="addItem" class="m-2 bg-blue-500 hover:bg-blue-700
+				text-white font-bold py-2 px-4 rounded">Add</button>
+
+<div class="text-left bg-green-700 mr-auto ml-auto p-4 w-1/2" id="list">    
+  <ol>
+    <li v-for="(item, index) in items" :key="item.id">
+      {{ index + 1 }} . {{ item }}
+    </li>
+  </ol>
+</div>
 
 </template>
 
@@ -24,5 +27,5 @@
   }
   items.value.push(newItem.value);
   newItem.value="";
-}
+  }
 </script>
